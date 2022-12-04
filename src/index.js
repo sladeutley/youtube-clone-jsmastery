8df8ee9b96394a94ef5cf8c9be54eb0d606341cb
sladeutley <slade.utley@gmail.com>
2022-12-04 12:07:21 -0800

    adjusted index.js to render App.js and  adapt to react v18

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); //create root of our application - this is the only place in entire app where we're fetching from dom
+
+root.render(<App />);
