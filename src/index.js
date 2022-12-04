df2b4fccf2534f4f6328616fb421585c0e4b3afd
sladeutley <slade.utley@gmail.com>
2022-12-04 11:48:03 -0800

    added App.js and index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root')); //create root of our application - this is the only place in entire app where we're fetching from dom
