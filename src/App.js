2dbe55ccd68dd88ef1ecfe73a7ee752b23c648cb
sladeutley <slade.utley@gmail.com>
2022-12-04 12:14:55 -0800

    start building structure of entire app. Start with importing then got rid of return to have 'an instant return'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material'; //box is a div
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+  <div>App</div>
+)
 
 export default App
