5d94ad374bdff10be02735fbc92b100f2e04d7d3
sladeutley <slade.utley@gmail.com>
2022-12-04 12:11:00 -0800

    imported css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); //create root of our application - this is the only place in entire app where we're fetching from dom
 
