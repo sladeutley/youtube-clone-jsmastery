86055e5e4a25d41d059bed7080e74d0435144961
sladeutley <slade.utley@gmail.com>
2022-12-05 09:06:22 -0800

    import dependencies for navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { Stack } from '@mui/material' //to see what a component does, hover over it, and click on demo
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
