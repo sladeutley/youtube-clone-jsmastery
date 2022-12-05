243eced4de854aea062584f5d0850ec756cd461d
sladeutley <slade.utley@gmail.com>
2022-12-05 10:39:38 -0800

    Started Feed component/layout and made correction to App.js

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
-const Feed = () => {
+const Feed = () => { //not sure why we keep curly braces here and don't do a direct return?
   return (
-    <div>Feed</div>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row"} }}>
+      {/* Above, flex direction means usually lets have it as a column, but on medium devices like phones, lets do it as a row */}
+      <Box sx={{ height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2} }}>
+        Sidebar
+
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff'}}>
+          Copyright 2022 U&G Holdings
+        </Typography>
+      </Box>
+    </Stack>
   )
 }
 
