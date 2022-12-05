b9ba4776e6d96b1df3d5e92c511bfc505401bec3
sladeutley <slade.utley@gmail.com>
2022-12-05 10:46:36 -0800

    create Sidebar component and link it up

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
+import Sidebar from './Sidebar';
 
 const Feed = () => { //not sure why we keep curly braces here and don't do a direct return?
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row"} }}>
       {/* Above, flex direction means usually lets have it as a column, but on medium devices like phones, lets do it as a row */}
       <Box sx={{ height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2} }}>
-        Sidebar
-
+        <Sidebar />
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff'}}>
           Copyright 2022 U&G Holdings
         </Typography>
