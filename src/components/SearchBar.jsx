259cf5f49f5a56888272c21a61a8662f90e587a5
sladeutley <slade.utley@gmail.com>
2022-12-05 09:52:36 -0800

    created SearchBar layout

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import { useState} from 'react'; //this is a hook - google what this is.
+import { useNavigate } from 'react-router-dom'; //this is a hook too
+import { Paper, IconButton } from '@mui/material'; //this is a component from material ui
+import { Search } from '@mui/icons-material';
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    // Below, Paper is just a div with white background and looks elevated 
+    <Paper
+      component="form"
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        border: '1px solid #e3e3e3',
+        pl: 2,
+        boxShadow: 'none',
+        mr: { sm: 5 }
+      }}
+    >
+      <input 
+        className="search-bar"
+        placeholder="Search..."
+        value=""
+        onChange={() => {}}
+      />
+      <IconButton 
+        type="submit" 
+        sx={{ 
+          p: '10px', 
+          color: 'red' 
+        }}
+      >
+        <Search />
+      </IconButton>
+    </Paper>
   )
 }
 
