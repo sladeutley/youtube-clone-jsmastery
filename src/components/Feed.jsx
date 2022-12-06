6d8a916e3b0651bd53f93e32aacfb185ca704e1f
sladeutley <slade.utley@gmail.com>
2022-12-05 18:51:12 -0800

    put fetFromAPI function in feed using useEffect. Made 'selectedCategory' part dynamic bc this is what's going to go in sidebar when user searches videos by selecting category. Also, adjusted url variable in fetchFromAPI.js file to not have 'search' in it bc we have that part of url in Feed.jsx now

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,20 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
+import { fetchFromAPI } from '../util/fetchFromAPI';
 import { Sidebar, Videos } from './';
 // import { Sidebar, Videos } from './components'; // This is same as above, but don't have to put "from './Components'" bc we're already in Components folder
 
 // Fetching videos - go to https://rapidapi.com/ytdlfree/api/youtube-v31/ -> click 'Subscribe to Test' button, sign up for free plan, then go back to 'Endpoints' tab -> click on 'GET Suggested Videos' on left of screen, copy options object on right of screen (starts with 'const options') then create 'fetchFromAPI.js' within 'utils' folder, and in that file import axios like i've done in the code, and past the options object.
 
 const Feed = () => { //not sure why we keep curly braces here and don't do a direct return?
+  
+  // to use this function, we'll implement useEffect, which is a lifecycle hook that gets called when component initially loads (which is what we want - want videos to load on this page). Also, have to provide a dependency array which for now we'll leave empty (meaning code within this function will only load when we reload the page) 
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`) //q is query
+  }, []);
+  
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row"} }}>
       {/* Above, flex direction means usually lets have it as a column, but on medium devices like phones, lets do it as a row */}
