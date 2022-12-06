3e7263d2486794db5bd9a5a9b14d698c03f6853e
sladeutley <slade.utley@gmail.com>
2022-12-05 18:19:05 -0800

    deleted 'GET' request in options object bc don't need bc we're doing that in axios, and delete url bc we are going to put in special variable. Also, we don't need to pass any params just yet, we just need maxResults: '50' right now. Last, need to move key to environment variables so safe.

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,16 +1,14 @@
 import axios from 'axios';
 
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com/search';
+
 const options = {
-  method: 'GET',
-  url: 'https://youtube-v31.p.rapidapi.com/search',
+  url: BASE_URL,
   params: {
-    relatedToVideoId: '7ghhRHRP6t4',
-    part: 'id,snippet',
-    type: 'video',
     maxResults: '50'
   },
   headers: {
-    'X-RapidAPI-Key': 'f160caff59msh591ba284faf6842p1f91ddjsn9933c1653a96',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   }
 };
