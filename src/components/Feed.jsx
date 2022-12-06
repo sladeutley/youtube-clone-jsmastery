615debaec0f1294436593765cc32de07f454f81a
sladeutley <slade.utley@gmail.com>
2022-12-05 18:59:25 -0800

    definte useState field to fetch videos for category. corrected 'util' to 'utils' folder path.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,19 +1,21 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
-import { fetchFromAPI } from '../util/fetchFromAPI';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 import { Sidebar, Videos } from './';
 // import { Sidebar, Videos } from './components'; // This is same as above, but don't have to put "from './Components'" bc we're already in Components folder
 
 // Fetching videos - go to https://rapidapi.com/ytdlfree/api/youtube-v31/ -> click 'Subscribe to Test' button, sign up for free plan, then go back to 'Endpoints' tab -> click on 'GET Suggested Videos' on left of screen, copy options object on right of screen (starts with 'const options') then create 'fetchFromAPI.js' within 'utils' folder, and in that file import axios like i've done in the code, and past the options object.
 
 const Feed = () => { //not sure why we keep curly braces here and don't do a direct return?
+
+  const [selectedCategory, setSelectedCategory] = useState('New'); //at start it's set to new
   
   // to use this function, we'll implement useEffect, which is a lifecycle hook that gets called when component initially loads (which is what we want - want videos to load on this page). Also, have to provide a dependency array which for now we'll leave empty (meaning code within this function will only load when we reload the page) 
 
   useEffect(() => {
     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`) //q is query
-  }, []);
+  }, [selectedCategory]); //since we're using 'selectedCategory' within useEffect, we also need to add it to the dependency array here. This means it's going to call this function every time we change the category.
   
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row"} }}>
