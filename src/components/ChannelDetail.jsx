70122fb7d8a282162b6d4f5f55f6a4cefb62d6b7
sladeutley <slade.utley@gmail.com>
2022-12-12 09:45:44 -0800

    fetch videos belonging to a channel. Now we know data is coming back, and we're storing it in the state

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -7,14 +7,17 @@ import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
   const [channelDetail, setChannelDetail] = useState(null); //sets channel detail. At start set to null - don't know why
+  const [videos, setVideos] = useState([]) // don't know why we set to empty array at start instead of null
 
   const { id } = useParams(); //need channel id and this is how we get access to the channel id
 
-  console.log(channelDetail);
+  console.log(channelDetail, videos);
 
   useEffect(() => { //going to render as soon as component opens
-    fetchFromAPI(`channels?part="snippet&id=${id}`)
-      .then((data) => setChannelDetail(data?.items[0]));
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+      .then((data) => setChannelDetail(data?.items[0])); //this reads as setChannelDetail which is equal to data?.items[0]
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+      .then((data) => setVideos(data?.items)); //this reads as setVideos which is equal to data?.items
   }, [id]) //[id] makes it so this hook will render as well whenever the id changes
 
   return (
