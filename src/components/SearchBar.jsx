d73d8bffb901acbc32b44209c42aff47c4d376f9
sladeutley <slade.utley@gmail.com>
2022-12-12 10:56:02 -0800

    adjust code in SearchFeed for it's unique functionality. Also, had to adjust navbar to allow user input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,11 +4,25 @@ import { Paper, IconButton } from '@mui/material'; //this is a component from ma
 import { Search } from '@mui/icons-material';
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const navigate = useNavigate(); //initialize useNavigate
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm(''); //reset searchTerm to empty string
+    }
+  }
+
   return (
     // Below, Paper is just a div with white background and looks elevated 
     <Paper
       component="form"
-      onSubmit={() => {}}
+      onSubmit={handleSubmit} //this passes the searchTerm user inputs below to our url/query
       sx={{
         borderRadius: 20,
         border: '1px solid #e3e3e3',
@@ -20,8 +34,8 @@ const SearchBar = () => {
       <input 
         className="search-bar"
         placeholder="Search..."
-        value=""
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)} //the 'e' sets a keypress event
       />
       <IconButton 
         type="submit" 
