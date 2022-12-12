015ca903ec0a50259219eac6f21622d67fd7a09f
sladeutley <slade.utley@gmail.com>
2022-12-12 09:26:44 -0800

    added fetchFromAPI function within useEffect hook

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -3,12 +3,14 @@ import { useParams } from 'react-router-dom';
 import { Box } from "@mui/material";
 
 import { Videos, ChannelCard } from "./"; //adding this so can reuse these components that we have already created here (with making some minor adjustments) to make things easier 
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
   const { id } = useParams(); //need channel id and this is how we get access to the channel id
 
   useEffect(() => { //going to render as soon as component opens
-
+    fetchFromAPI(`channels?part="snippet&id=${id}`)
+      .then((data) => )
   }, [id]) //[id] makes it so this hook will render as well whenever the id changes
 
   return (
