dc80ea6293df18f9ed0f25a5f309df3aacd4faee
sladeutley <slade.utley@gmail.com>
2022-12-12 09:22:31 -0800

    start creating useEffect hook within this component

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -7,6 +7,10 @@ import { Videos, ChannelCard } from "./"; //adding this so can reuse these compo
 const ChannelDetail = () => {
   const { id } = useParams(); //need channel id and this is how we get access to the channel id
 
+  useEffect(() => { //going to render as soon as component opens
+
+  }, [id]) //[id] makes it so this hook will render as well whenever the id changes
+
   return (
     <div>{id}</div>
   )
