287707e212c4b78993f9df7bab7adc0496d88d96
sladeutley <slade.utley@gmail.com>
2022-12-13 10:05:36 -0800

    added suggested videos rendering

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -4,7 +4,7 @@ import ReactPlayer from 'react-player'; //this is a dependency that allows us to
 import { Typography, Box, Stack } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 
-import { Video } from './';
+import { Videos } from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 
@@ -56,6 +56,11 @@ const VideoDetail = () => {
           </Box>
         </Box>
       </Stack>
+
+      <Box px={2} py={{md: 1, xs: 5}} justifyContent="center" alignItems="center">
+        <Videos videos={videos} />
+      </Box>
+
     </Box>
   )
 }
