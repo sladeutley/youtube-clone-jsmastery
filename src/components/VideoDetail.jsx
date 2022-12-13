35a4f3c1cfe7a9ab65f70d3a0c259b463305f611
sladeutley <slade.utley@gmail.com>
2022-12-13 09:06:56 -0800

    added title below video

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -23,6 +23,9 @@ const VideoDetail = () => {
         <Box flex={1}>
           <Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
             <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
+            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+              {videoDetail.snippet.title}
+            </Typography>
           </Box>
         </Box>
       </Stack>
